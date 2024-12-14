ee380b47fda0b7bd15413ce1b6bb1617c1fb0345
iam-junedali <junedali.sk@gmail.com>
2024-12-14 14:47:43 +0530

    Commit and Push

--- Variables.sh
+++ Variables.sh
@@ -3,4 +3,4 @@ echo Welcome to the bash scripting and
 name="AWS"
 environment="dev"
 echo "My Cloud is $name and we are using cloud environment for $environment clusters."
-"we are at $HOME and today is " && date
+echo "----------------------Welcome to AWS-------------------------------------------"
